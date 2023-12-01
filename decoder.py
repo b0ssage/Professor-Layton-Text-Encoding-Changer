d5747a89dffe40ecda669cdd0d9767fd1de99135
Andrew Gordon <andrew.c.gordon4@gmail.com>
2023-12-01 19:49:07 +0000

    Completed lt2 + lt3
    Fully changed the encoding for all text files in DB/PB and UW/LF

--- decoder.py
+++ decoder.py
@@ -1,56 +1,77 @@
+import tkinter
+from tkinter import filedialog
 import time
 import os
 from os import listdir
+from pathlib import Path
 
-# directory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)")
-# with open("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\output.txt", "a") as f:
+# directory = os.listdir(inputPath)
+# with open("C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\output.txt", "a") as f:
 #     for i in directory:
 #         print(i, file=f)
-#         subDirectory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)" + "\\" + i)
+#         subDirectory = os.listdir(inputPath + "\\" + i)
 #         for j in subDirectory:
 #             print(j, file=f)
 
-directory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)")
+tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
+inputPath = filedialog.askdirectory()
+# relative = Path(userSelection)
+# inputPath = relative.absolute()
+
+# print("#############################################################################")
+# print("\n")
+# print("Selected:")
+# print(inputPath)
+# print("\n")
+# print("#############################################################################")
+# time.sleep(2)
+
+# inputPath = "C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\lt2\\lt2 - JP - Diabolical Box\\data_jp"
+outputPath = "C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed"
+
+# directory = os.listdir(inputPath)
+directory = [d for d in os.listdir(inputPath) if os.path.isdir(os.path.join(inputPath, d))]
+print(directory)
+
 noOfFolders = 0
 noOfFiles = 0
 
 for i in directory:
     noOfFolders = noOfFolders + 1
-    subDirectory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)" + "\\" + i)
+    subDirectory = os.listdir(inputPath + "\\" + i)
     for j in subDirectory:
         noOfFiles = noOfFiles + 1
 
-newpath = "C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed"
 
-if os.path.exists(newpath) == True:
-    print("Already exists: processed")
+if os.path.exists(outputPath) == True:
+    print("Already exists: /processed")
     time.sleep(0.5)
 else:
-    os.makedirs(newpath)
+    os.makedirs(outputPath)
 
-# directory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)")
+# directory = os.listdir(inputPath)
 completedFolders = 0
 completedFiles = 1
 
 for i in directory:
-    newpath = "C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed" + "\\" + i
-    if os.path.isdir(newpath) == True:
-        print(f"Already exists: processed/{i}")
+    outputPath = "C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed" + "\\" + i
+    if os.path.isdir(outputPath) == True:
+        print(f"Already exists: /processed/{i}")
         completedFolders = completedFolders + 1
         time.sleep(0.5)
     else:
-        os.makedirs(newpath)
+        os.makedirs(outputPath)
         completedFolders = completedFolders + 1
     
-        subDirectory = os.listdir("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)" + "\\" + i)
+        subDirectory = os.listdir(inputPath + "\\" + i)
         for j in subDirectory:
-            fileCheck = "C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\{i}\\{j}"
+            fileCheck = "C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\{i}\\{j}"
             if os.path.isfile(fileCheck) == True:
                 print(f"Already exists: processed/{i}/{j}")
                 completedFiles = completedFiles + 1
             else:
-                file1 = open(f"C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)\\{i}\\{j}", "r", encoding = "shiftjis")
-                file2 = open(f"C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\{i}\\{j}", "w", encoding="utf8")
+                file1 = open(f"{inputPath}\\{i}\\{j}", "r", encoding = "shiftjis")
+                file2 = open(f"C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\{i}\\{j}", "w", encoding="utf8")
                 l = file1.readline()
                 while l:
                     file2.write(l)
@@ -62,8 +83,8 @@ for i in directory:
 
 
 
-# file1 = open("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)\\etext\\e1_t0.txt", "r", encoding = "shiftjis")
-# file2 = open("C:\\Users\\YOUR_USER_HERE\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\e1_t0.txt", "w", encoding="utf8")
+# file1 = open("C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\original files (Shift-JIS encoding)\\etext\\e1_t0.txt", "r", encoding = "shiftjis")
+# file2 = open("C:\\Users\\andre\\Documents\\VSCode\\Professor-Layton-Text-Encoding-Changer\\processed\\e1_t0.txt", "w", encoding="utf8")
 # l = file1.readline()
 # while l:
 #     file2.write(l)
